328c7b931dc64bdfafcb31b84ef620d53b3bf9fd
avishkardixit <75980975+avishkardixit@users.noreply.github.com>
2025-01-22 18:03:36 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+// Start server
+// Request to server
+// Receive request
+// Parse request
+// Retrieve data from database
+// Send data back to client
+// Close server
+// End
